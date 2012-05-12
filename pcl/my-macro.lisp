1ab1182f6f8d3eb5cb1385235a9991f2186fc7a8
chen feng <mailtocf@gmail.com>
2012-05-13 01:59:35 +0800

    add once-only and other macro

--- pcl/my-macro.lisp
+++ pcl/my-macro.lisp
@@ -1,4 +1,11 @@
 ;;;;create my own macro for test
+(defmacro once-only ((&rest names) &body body)
+  (let ((gensyms (loop for n in names collect (gensym))))
+    `(let (,@(loop for g in gensyms collect `(,g (gensym))))
+       `(let (,,@(loop for g in gensyms for n in names collect ``(,,g ,,n)))
+	  ,(let (,@(loop for n in names for g in gensyms collect `(,n ,g)))
+	     ,@body)))))
+
 (defmacro with-gensym ((&rest name) &body body)
   `(let ,(loop for n in name collect `(,n (gensym)))
      ,@body))
@@ -11,8 +18,15 @@
   (loop for n from number when (prime n) return n))
 
 (defmacro do-primes ((var start end) &body body)
-  (with-gensym (end-name)
-    `(do ((,var (next-prime ,start) (next-prime (1+ ,var)))
-	  (,end-name ,end))
-	 ((> ,var ,end-name))
+  (once-only (start end)
+    `(do ((,var (next-prime ,start) (next-prime (1+ ,var))))
+	 ((> ,var ,end))
        ,@body)))
+
+(defmacro dp-with-newlines (number lines)
+  (once-only (number lines)
+    `(let ((n 0))
+       (do-primes (p 0 ,number)
+	 (when (zerop (mod (setf n (1+ n)) ,lines))
+	   (format t "~%"))
+	 (format t "~a ~3t" p)))))
